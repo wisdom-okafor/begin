ed15da23d04b0c21c3a0e89dcd908f6a52692d42
wisdom okafor <okaforw1@gmail.com>
2022-09-05 13:45:57 +0000

    Add existing file

---
+++ bio/bio.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func mydetails(name string, age int, phonenumber string, height float64) string {
+
+//wisdom okafor, 19, +1237035392310, 5'7
+
+    var result = fmt.Sprintf("My name is %s, i am %d years old, my phonenumber is %s, and my height is %f", name, age, phonenumber, height)
+	return result 
+}
+	func main () {
+    var result = mydetails("wisdom okafor", 19, "+2347035392310", 5.7)
+	fmt.Println(result)
+
+}
+
