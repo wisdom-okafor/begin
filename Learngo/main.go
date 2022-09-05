55cc4b941eee7ffd3d3cea6dab283d5b9ac6f0f9
wisdom okafor <okaforw1@gmail.com>
2022-09-05 22:49:00 +0000

    Add existing file

--- Learngo/main.go
+++ Learngo/main.go
@@ -5,10 +5,10 @@ import (
 )	
 func main() {
         a := []int{1, 2, 3}
-//		b := a 
-//		b[1] = 5
+		b := a 
+		b[1] = 5
 		fmt.Println(a)
-//		fmt.Println(b)
+		fmt.Println(b)
 		fmt.Printf("Lenght: %v\n", len(a))
 		fmt.Printf("capacity: %v\n", cap(a)) 
 }
