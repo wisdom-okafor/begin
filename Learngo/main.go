b9ce1748567f8ef7f00e72731da1b87163fb1021
wisdom okafor <okaforw1@gmail.com>
2022-09-05 18:03:25 +0000

    Add existing file

--- Learngo/main.go
+++ Learngo/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 )	
 func main() {
-	var students [3]string
-	fmt.Printf("students: %v\n", students)
-	students[0] = "Lisa"
-	students[1] = "Ahmed"
-	students[2] = "Arnold"
-	fmt.Printf("students: %v\n", students)
+         var students [5]string
+		 fmt.Printf("Students: %v\n", students)
+		 students[0] = "Lisa"
+		 students[2] = "Ahmed"
+		 students[1] = "Arnold"
+		 fmt.Printf("Students #1: %v\n", students [1])
+		 fmt.Printf("Number of students: %v\n", len(students))	
   
 }
