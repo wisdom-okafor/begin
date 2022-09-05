02b4da16796eaefcf01ca910e85617a9fe9194f1
wisdom okafor <okaforw1@gmail.com>
2022-09-05 23:14:31 +0000

    Add existing file

--- Learngo/main.go
+++ Learngo/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )	
 func main() {
-        a := make([]int, 3)
+        a := make([]int, 3, 100)
 		fmt.Println(a)
 		fmt.Printf("Length: %v\n", len(a))
 		fmt.Printf("capacity: %v\n", cap(a))
