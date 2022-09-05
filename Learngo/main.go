7410c8d4f8a576ef361035f69cd9b11a87f8edf1
wisdom okafor <okaforw1@gmail.com>
2022-09-05 22:38:25 +0000

    Add existing file

--- Learngo/main.go
+++ Learngo/main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 )	
 func main() {
-        a := [...]int{1, 2, 3}
-		b := a 
-		b[1] = 5
+        a := []int{1, 2, 3}
+//		b := &a 
+//		b[1] = 5
 		fmt.Println(a)
-		fmt.Println(b)  
+//		fmt.Println(b)
+		fmt.Printf("Lenght: %v\n", len(a)) 
 }
