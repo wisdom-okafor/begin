ec505c84e34b04fbfcb3cda5127d70267d4d613c
wisdom okafor <okaforw1@gmail.com>
2022-09-05 23:00:30 +0000

    Add existing file

--- Learngo/main.go
+++ Learngo/main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 )	
 func main() {
-        a := []int{1, 2, 3}
-		b := a 
-		b[1] = 5
+        a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		b := a[:]      // slice of all elements
+		c := a[3:]     // slice from 4th element to end
+		d := a[:6]     // slicce first 6 elements
+		e := a[3:6]    // slice the 4th, 5th, and 6th elements
 		fmt.Println(a)
 		fmt.Println(b)
-		fmt.Printf("Lenght: %v\n", len(a))
-		fmt.Printf("capacity: %v\n", cap(a)) 
+		fmt.Println(c)
+		fmt.Println(d)
+		fmt.Println(e)
 }
