5ced4861b8b50a7105342817c231a56d15ae5acf
wisdom okafor <okaforw1@gmail.com>
2022-09-06 19:00:40 +0000

    Add existing file

--- Learngo/main.go
+++ Learngo/main.go
@@ -3,22 +3,7 @@ import (
 	"fmt" 
 )
 
-type Doctor struct {
-	number int
-	actorName string
-	companions []string
-}
-
 func main() { 
-	aDoctor:= Doctor{
-		number: 3,
-		actorName: "jon pertwee",
-		companions: []string {
-			"Liz shaw",
-			"Jo Grant",
-			
-			"Sarah Jane Smith",
-		},
-	}
-	fmt.Println(aDoctor.companions[1])
+	aDoctor:= struct{name string}{name: "John Pertwee"}
+	fmt.Println(aDoctor)	
 }	
