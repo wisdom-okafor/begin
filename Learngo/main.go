7a358024d65967527c0c96fb4d8677a44c03763c
wisdom okafor <okaforw1@gmail.com>
2022-09-07 22:11:19 +0000

    Add existing file

--- Learngo/main.go
+++ Learngo/main.go
@@ -1,14 +1,16 @@
 package main
 import (
 	"fmt"
-	"math"
 )
 func main() {
-	myNum := 0.123
-	if math.Abs(myNum / math.Pow(math.Sqrt(myNum), 2) - 1) < 0.001{
-		fmt.Println("These are the same")
+	switch 2 {
+		case 1:
+			fmt.Print("one")
+		case 2:
+			fmt.Println("two")
+		default:
+			fmt.Print("not one or two")
 
-	} else {
-		fmt.Println("These are different")
+	
 		}
 	}
