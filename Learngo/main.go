96e253e6ea8e856b0a8e13384a8b11369a8957dd
wisdom okafor <okaforw1@gmail.com>
2022-09-08 12:34:34 +0000

    Add exiting file

--- Learngo/main.go
+++ Learngo/main.go
@@ -3,16 +3,8 @@ import (
 	"fmt"
 )
 func main() {
-	statepopulations := map[string]int{
-		"California":     39250017,
-		"Texas":          27862596,
-		"Florida":        20612439,
-		"New york":       19745289,
-		"Pennsylvania":   12802503,
-		"Illinois":       12801539,
-		"Ohio":           11614373,
-	}
-	for k, v := range statepopulations {
+	s := "hello Go"
+	for k, v := range s {
 		fmt.Println(k, v)
 	}
 }
