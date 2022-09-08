ad36185e4148021c3e7e7dcfe9d403b6ba535cbc
wisdom okafor <okaforw1@gmail.com>
2022-09-08 11:10:54 +0000

    Add existing file

--- Learngo/main.go
+++ Learngo/main.go
@@ -3,17 +3,8 @@ import (
 	"fmt"
 )
 func main() {
-	var i interface{} = 1
-	switch i. (type) {
-	case int:
-			fmt.Println("i is an int")
-			break
-			fmt.Println("This will print too")
-	case float64:
-			fmt.Println("i is a float64")
-	case string:
-		fmt.Println("i is a string")
-	default:
-			fmt.Print("i is another type")
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	
 		}
 	}
