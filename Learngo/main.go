9ca69d982125bc73414a072a839bff889d2b4f65
wisdom okafor <okaforw1@gmail.com>
2022-09-08 12:04:25 +0000

    Add exiting file

--- Learngo/main.go
+++ Learngo/main.go
@@ -3,11 +3,12 @@ import (
 	"fmt"
 )
 func main() {
+	Loop:
 	for i := 1; i <= 3; i++ {
 		for j := 1; j <= 3; j++ {
 	fmt.Println(i * j)
 	if i * j >= 3 {
-		break
+		break Loop
 	}
             }
        }
