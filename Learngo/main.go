3e55f5b11fdcbfc31a49d150c2257bb0ac257536
wisdom okafor <okaforw1@gmail.com>
2022-09-08 11:03:28 +0000

    Add existing file

--- Learngo/main.go
+++ Learngo/main.go
@@ -7,12 +7,11 @@ func main() {
 	switch i. (type) {
 	case int:
 			fmt.Println("i is an int")
+			fmt.Println("This will print too")
 	case float64:
 			fmt.Println("i is a float64")
 	case string:
 		fmt.Println("i is a string")
-	case [3] int:
-		fmt.Println("i is [2]int")
 	default:
 			fmt.Print("i is another type")
 		}
