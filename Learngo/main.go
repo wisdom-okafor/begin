7e27e027e68b6dbe98c9738b47bb13e8ca20c35b
wisdom okafor <okaforw1@gmail.com>
2022-09-08 22:48:03 +0000

    Pointer arithemetic

--- Learngo/main.go
+++ Learngo/main.go
@@ -4,8 +4,9 @@ import (
 )
 
 func main() {
-  var a int = 42
-   var b *int = &a
-   fmt.Println(&a, b)
+  a := [3]int{1, 2, 3}
+  b := &a[0]
+  c := &a[1]
+   fmt.Printf("%v %p %p\n", a, b, c)
 	
 }
