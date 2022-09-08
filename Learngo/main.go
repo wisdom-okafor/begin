27d7798e3442a33d6f349c4664156fe2b8451219
wisdom okafor <okaforw1@gmail.com>
2022-09-08 14:55:29 +0000

    Pointer

--- Learngo/main.go
+++ Learngo/main.go
@@ -1,21 +1,10 @@
 package main
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 )
-func main() {
-	 res, err := http.Get("http://www.google.com/robots.txt")
-	 if err != nil {
-		log.Fatal(err)
-	 }
-	 robots, err := ioutil.ReadAll(res.Body)
-	 res.Body.Close()
-	 if err != nil {
-		log.Fatal(err)
-	 }
 
-	 fmt.Printf("%s", robots)
+func main() {
+   a := 42
+   fmt.Println(a)
 	
 }
