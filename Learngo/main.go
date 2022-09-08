5b2e117f51e7c2bb9dbdeaa37f2dceb9be4b4da8
wisdom okafor <okaforw1@gmail.com>
2022-09-08 13:37:12 +0000

    Demo app

--- Learngo/main.go
+++ Learngo/main.go
@@ -1,10 +1,21 @@
 package main
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 func main() {
-	 fmt.Println("start")
-	 defer fmt.Println("middle")
-	 fmt.Println("end")
+	 res, err := http.Get("http://www.google.com/robots.txt")
+	 if err != nil {
+		log.Fatal(err)
+	 }
+	 robots, err := ioutil.ReadAll(res.Body)
+	 res.Body.Close()
+	 if err != nil {
+		log.Fatal(err)
+	 }
+
+	 fmt.Printf("%s", robots)
 	
 }
