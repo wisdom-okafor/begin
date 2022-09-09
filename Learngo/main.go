23e0b423476ff1cac077760565212f3556605dc3
wisdom okafor <okaforw1@gmail.com>
2022-09-09 02:58:23 +0000

    Add existing file

--- Learngo/main.go
+++ Learngo/main.go
@@ -8,17 +8,18 @@ var wg = sync.WaitGroup{}
 
 func main() {
 	ch := make(chan int)
+	for j := 0; j < 5; j++ {
 	wg.Add(2)
-	go func() {
-		i := <- ch
+	go func () {
+		i := <-ch
 		fmt.Println(i)
 		wg.Done()
+		
 	}()
-	go func () {
-		i := 42
-		ch <- i
-		i = 27
+	go func() {
+		ch <- 42
 		wg.Done()
 	}()
-	wg.Wait()
+}
+wg.Wait()
 }
