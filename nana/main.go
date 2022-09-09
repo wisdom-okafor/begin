8e7b8ae9f82e615440a9ebd1d2a1fbe2b76f1627
wisdom okafor <okaforw1@gmail.com>
2022-09-09 22:30:02 +0000

    Add existing file

--- nana/main.go
+++ nana/main.go
@@ -11,5 +11,12 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	// ask for their name
+
+	userName = "Tom"
+	var userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
 	
 }	
