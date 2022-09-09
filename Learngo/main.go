d34de250fea48666181fa8ddefe409fda628538d
wisdom okafor <okaforw1@gmail.com>
2022-09-09 02:13:28 +0000

    Interface

--- Learngo/main.go
+++ Learngo/main.go
@@ -1,21 +1,17 @@
 package main
 import (
 	"fmt"
-	
 )
-func main () {
-	g := greeter{
-		greeting: "Hello",
-		name: "Go",
+
+func main() {
+	var i interface{} = 0
+	switch i.(type) {
+	case int:
+		fmt.Println("i is an integer")
+	case string:
+		fmt.Println("i is a string")
+	default:
+		fmt.Println("I don't know what i is")
 	}
-	g.greet()
-	fmt.Println("The new name is:", g.name)
-	}
-	type greeter struct {
-		greeting string
-		name string
-	}
-	func (g greeter) greet() {
-		fmt.Println(g.greeting, g.name)
-	}
+}
 	
