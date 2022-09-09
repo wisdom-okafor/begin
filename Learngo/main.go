f8d4036dfb155b852aa7d97c496d9d2b13f40a5a
wisdom okafor <okaforw1@gmail.com>
2022-09-09 11:18:40 +0000

    Buffer

--- Learngo/main.go
+++ Learngo/main.go
@@ -1,25 +1,61 @@
 package main
-
 import (
-	"fmt"
-	"sync"
+    "fmt"
+    "bytes"
 )
-var wg = sync.WaitGroup{}
+func main () {
+    var wc WriterCloser = NewBufferedWriterCloser()
+    wc.Write([]byte("Hello YouTube listeners, this is a test"))
+    wc.Close()
+}
 
-func main() {
-	ch := make(chan int, 50)
-	wg.Add(2)
-	go func(ch <-chan int) {
-		i := <-ch
-		 fmt.Println(i)
-		 i = <-ch
-		 fmt.Println(i)
-		wg.Done()
-	}(ch)
-	go func(ch chan<- int) {
-		ch <- 42
-		ch <- 27
-		wg.Done()
-	}(ch)
-	wg.Wait()
+type Writer interface {
+    Write([]byte) (int, error)
+}
+
+type Closer interface {
+    Close() error
+}
+
+type WriterCloser interface {
+    Writer
+    Closer
+}
+
+    type BufferedWriterCloser struct {
+        buffer *bytes.Buffer
+    }
+    func (bwc *BufferedWriterCloser) Write(data []byte) (int, error) {
+        n, err := bwc.buffer.Write(data)
+        if err != nil {
+            return 0, err
+        }
+        v := make ([]byte, 8)
+        for bwc.buffer.Len() > 8 {
+            _, err := bwc.buffer.Read(v)
+            if err != nil {
+            return 0, err
+        }
+        _, err = fmt.Println(string(v))
+        if err != nil {
+    return 0, err
+        }
+    }
+    return n, nil
+}
+func (bwc *BufferedWriterCloser) Close() error {
+    for bwc.buffer.Len() > 0 {
+        data := bwc.buffer.Next(8)
+        _, err := fmt.Println(string(data))
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
+
+func NewBufferedWriterCloser() *BufferedWriterCloser {
+        return &BufferedWriterCloser{
+            buffer: bytes.NewBuffer([]byte{}),
+        }
 }
