66ecdb403b7a7befa092fc34d54c9b4d306241fa
wisdom okafor <okaforw1@gmail.com>
2022-09-09 18:14:21 +0000

    Update conference

--- nana/main.go
+++ nana/main.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go conference"
+	const conferenceTickets = 50 
+	var remainingTickets = 50 
 
 	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println ("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
 
 	
