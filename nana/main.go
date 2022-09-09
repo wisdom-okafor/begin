a95419f5afe82d832d4d2b167d9491081ae0ef6f
wisdom okafor <okaforw1@gmail.com>
2022-09-09 22:49:49 +0000

    update printf

--- nana/main.go
+++ nana/main.go
@@ -7,15 +7,18 @@ func main() {
 	const conferenceTickets = 50 
 	var remainingTickets = 50 
 
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 	var userName string
-	// ask for their name
+	var userTickets int
+	// ask user for their name
 
 	userName = "Tom"
-	var userTickets = 2
+	userTickets = 2
 	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 	
