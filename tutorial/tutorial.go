1511787f2b7522c705be3bd0ba1b8ef04c060a99
wisdom okafor <okaforw1@gmail.com>
2022-09-20 14:33:53 +0100

    string

--- tutorial/tutorial.go
+++ tutorial/tutorial.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 )
 
 var pl = fmt.Println
 
 func main() {
-	iAge := 8
-	if (iAge >= 1) && (iAge <=18) {
-		pl("Important Birthday")
-	} else if (iAge == 21) || (iAge == 50) {
-		pl("Important Birthday")
-	} else if iAge >= 65 {
-		pl("Important Birthday")
-	} else {
-		pl("Not an Important Birthday")
-	}
-	pl("!true =", !true)
-	
+    sV1 := "A word"
+	replacer := strings.NewReplacer("A", "Another")
+	sV2 := replacer.Replace(sV1)
+	pl(sV2)
+	pl("Length :", len(sV2))
+	pl("Contains Another :", strings.Contains(sV2, "Another"))
+	pl("o index :", strings.Index(sV2, "o"))
+	pl("Replace :", strings.Replace(sV2, "o", "o", -1))
+	sV3 := "\nSome Work\n"
+	sV3 = strings.TrimSpace(sV3)
+	pl("Split :", strings.Split("a-b-c-d", "-"))
+	pl("Lower :", strings.ToLower(sV2))
+	pl("Upper :", strings.ToUpper(sV2))
+	pl("Prefix :", strings.HasPrefix("tacocat", "taco"))
+	pl("Suffix :", strings.HasSuffix("tacocat", "cat"))
 }
