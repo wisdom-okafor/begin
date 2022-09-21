28bcc4726943d47a1ba9d6d52ed52cac2bea4cdb
wisdom okafor <okaforw1@gmail.com>
2022-09-21 22:07:37 +0100

    Rune

--- tutorial/tutorial.go
+++ tutorial/tutorial.go
@@ -2,26 +2,16 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"unicode/utf8"
 
 )
 
 var pl = fmt.Println
 
 func main() {
-    sV1 := "A word"
-	replacer := strings.NewReplacer("A", "Another")
-	sV2 := replacer.Replace(sV1)
-	pl(sV2)
-	pl("Length :", len(sV2))
-	pl("Contains Another :", strings.Contains(sV2, "Another"))
-	pl("o index :", strings.Index(sV2, "o"))
-	pl("Replace :", strings.Replace(sV2, "o", "o", -1))
-	sV3 := "\nSome Work\n"
-	sV3 = strings.TrimSpace(sV3)
-	pl("Split :", strings.Split("a-b-c-d", "-"))
-	pl("Lower :", strings.ToLower(sV2))
-	pl("Upper :", strings.ToUpper(sV2))
-	pl("Prefix :", strings.HasPrefix("tacocat", "taco"))
-	pl("Suffix :", strings.HasSuffix("tacocat", "cat"))
+    rStr := "abcdefg"
+    pl("Rune Count :", utf8.RuneCountInString (rStr))
+	for i, runeVal := range rStr {
+		fmt.Printf("%d : %#U %c\n", i, runeVal, runeVal)
+	}
 }
