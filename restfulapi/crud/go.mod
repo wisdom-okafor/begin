cd8cb2ea4486f7e936dcf14e08d223ae9005ddca
wisdom okafor <okaforw1@gmail.com>
2022-09-27 14:01:48 +0100

    crud-api

---
+++ restfulapi/crud/go.mod
@@ -0,0 +1,5 @@
+module github.com/beginner/restfulapi/crud
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ restfulapi/crud/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Desc   string `json:"desc"`
+}
+
+var Books = []Book{
+	{
+		Id:     1,
+		Title:  "Golang",
+		Author: "Gopher",
+		Desc:   "A book for Go",
+	},
+}
+
+func GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(Books)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", GetAllBooks)
+
+	log.Println("API is running!")
+	http.ListenAndServe(":4000", router)
+}
