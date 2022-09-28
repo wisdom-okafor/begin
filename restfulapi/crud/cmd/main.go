7dd80943b2fd20efd758cb33cab366a63e564102
wisdom okafor <okaforw1@gmail.com>
2022-09-28 16:24:31 +0100

    Crud update book

--- restfulapi/crud/cmd/main.go
+++ restfulapi/crud/cmd/main.go
@@ -14,6 +14,7 @@ func main() {
 	router.HandleFunc("/books", handlers.GetAllBooks).Methods(http.MethodGet)
 	router.HandleFunc("/books/{id}", handlers.GetBook).Methods(http.MethodGet)
 	router.HandleFunc("/books", handlers.AddBook).Methods(http.MethodPost)
+	router.HandleFunc("/books/{id}", handlers.UpdateBook).Methods(http.MethodPut)
 
 	log.Println("API is running!")
 	http.ListenAndServe(":4000", router)
