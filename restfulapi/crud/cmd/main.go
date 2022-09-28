81e30e449df329b0f1ad525fb892a02a59baa90c
wisdom okafor <okaforw1@gmail.com>
2022-09-28 17:19:57 +0100

    Crud delete book

--- restfulapi/crud/cmd/main.go
+++ restfulapi/crud/cmd/main.go
@@ -15,6 +15,7 @@ func main() {
 	router.HandleFunc("/books/{id}", handlers.GetBook).Methods(http.MethodGet)
 	router.HandleFunc("/books", handlers.AddBook).Methods(http.MethodPost)
 	router.HandleFunc("/books/{id}", handlers.UpdateBook).Methods(http.MethodPut)
+	router.HandleFunc("/books/{id}", handlers.DeleteBook).Methods(http.MethodDelete)
 
 	log.Println("API is running!")
 	http.ListenAndServe(":4000", router)
